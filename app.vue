1ccd942dada08449c1b390050da7d051abfd7c85
qollaaa <131690765+qollaaa@users.noreply.github.com>
2023-09-18 10:22:59 -0400

    Update app.vue

--- app.vue
+++ app.vue
@@ -23,6 +23,7 @@
 			<form @submit.prevent="submitForm">
 				<input v-model="query" placeholder="Explore the web free!" />
 			</form>
+			     <a href="credits.html">CREDITS</a>
 		</div>
 		<div class="center" id="loading">
 			<div class="wave"></div>
